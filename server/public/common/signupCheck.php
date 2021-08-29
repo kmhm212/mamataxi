ebcfae90f8e7211fdad0ee74d0f385dbc9a893a3
kmhm212 <hmhm_auau@yahoo.co.jp>
2021-08-29 12:57:39 +0900

    会員情報登録画面作成・装飾

--- server/public/common/signupCheck.php
+++ server/public/common/signupCheck.php
@@ -14,10 +14,10 @@ $title = " | 会員登録";
 <?php include_once __DIR__ . '/_header.php' ?>
 
 <article>
-    <div class="loginParts signUp2 wrapper">
+    <div class="signUpCheck wrapper">
         <h2 class="subPageH2">会員登録</h2>
         <section>
-            <h3 class="subPageH3">会員情報を登録</h3>
+            <h3 class="subPageH3">以下の情報で登録</h3>
             <form action="" method="post">
                 <label for="name" class="label1">ユーザー名</label>
                 <input type="text" name="name" id="name" class="input1" placeholder="ユーザー名" required value="mamasan@taxi.com">
@@ -37,7 +37,7 @@ $title = " | 会員登録";
                 <input type="date" name="birth" id="birth" class="input3" placeholder="2021/9/5" required>
                 <label for="tel" class="label4">電話番号</label>
                 <input type="tel" name="tel" id="tel" class="input4" placeholder="000-0000-0000" required>
-                <input type="submit" value="確認">
+                <input type="submit" value="規約に同意して登録">
                 <!-- <a href="">戻る</a> -->
             </form>
             <a href="login.php">ログインはこちら</a>
