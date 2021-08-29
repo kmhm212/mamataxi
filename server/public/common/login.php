0276926c80f2e560d3e0623430e3414e5d6df8cf
kmhm212 <hmhm_auau@yahoo.co.jp>
2021-08-29 11:08:21 +0900

    ログイン画面作成・装飾

--- server/public/common/login.php
+++ server/public/common/login.php
@@ -1,4 +1,37 @@
 <?php
 
+$title = " | ログイン";
+
 ?>
 
+<!DOCTYPE html>
+<html lang="ja">
+
+<?php include_once __DIR__ . '/_head.php' ?>
+
+<body>
+
+<?php include_once __DIR__ . '/_header.php' ?>
+
+<article>
+    <div class="loginParts login wrapper">
+        <h2 class="subPageH2">ログイン</h2>
+        <section>
+            <h3 class="subPageH3">メールアドレスでログイン</h3>
+            <form action="" method="post">
+                <label for="email" class="label1">メールアドレス</label>
+                <input type="email" name="email" id="email" class="input1" placeholder="mamasan@taxi.com" required>
+                <label for="password" class="label2">パスワード</label>
+                <input type="password" name="password" id="password" class="input2" placeholder="パスワード" required>
+                <input type="submit" value="ログイン">
+            </form>
+            <a href="signup.php">会員登録はこちら</a><br>
+            <a href="">パスワードを忘れたりなんかしない</a>
+        </section>
+    </div>
+</article>
+
+<?php include_once __DIR__ . '/_footer.html' ?>
+
+</body>
+</html>
--- server/public/common/signup2.php
+++ server/public/common/signup2.php
@@ -14,18 +14,18 @@ $title = " | 会員登録";
 <?php include_once __DIR__ . '/_header.php' ?>
 
 <article>
-    <div class="loginParts signUp wrapper">
+    <div class="loginParts signupParts signUp wrapper">
         <h2 class="subPageH2">会員登録</h2>
         <section>
             <h3 class="subPageH3">メールアドレスで登録</h3>
             <form action="" method="post">
                 <label for="email" class="label1">メールアドレス</label>
-                <input type="email" name="email" id="email" class="input1" placeholder="mamasan@taxi.com" required>
+                <input type="email" name="email" id="email" class="input1" placeholder="例) mamasan@taxi.com" required>
                 <label for="password" class="label2">パスワード</label>
                 <input type="password" name="password" id="password" class="input2" placeholder="パスワード" required>
                 <label for="rePassword" class="label3">パスワード(確認)</label>
                 <input type="password" name="rePassword" id="rePassword" class="input3" placeholder="パスワード(確認)" required>
-                <input type="submit" value="次へ">
+                <input type="submit" value="規約に同意して登録">
             </form>
             <a href="login.php">ログインはこちら</a>
         </section>
